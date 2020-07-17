4f2040244c97c59d73f735cb9419d313f4bd5db6
Anshul Gupta <hi.anshulg@gmail.com>
2020-07-17 10:55:58 -0700

    open links in new tab

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,57 @@
-;(function () {
-	
-	'use strict';
+(function () {
+  "use strict";
 
-	var owlCarousel = function(){
+  var owlCarousel = function () {
+    new WOW().init();
 
-        new WOW().init();
+    $(".owl-carousel").owlCarousel({
+      items: 4,
+      loop: true,
+      margin: 170,
+      center: true,
+      smartSpeed: 900,
+      nav: true,
+      navText: [
+        "<i class='fa carousel-left-arrow fa-chevron-left'></i>",
+        "<i class='fa carousel-right-arrow fa-chevron-right'></i>",
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true,
+        },
+        600: {
+          items: 1,
+          nav: true,
+          margin: 120,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+          loop: true,
+          autoplay: true,
+          autoplayTimeout: 1500,
+          navText: [
+            "<i class='fa carousel-left-arrow fa-chevron-left'></i>",
+            "<i class='fa carousel-right-arrow fa-chevron-right'></i>",
+          ],
+        },
+      },
+    });
+  };
 
-        $('.owl-carousel').owlCarousel({
-            items : 4,
-            loop  : true,
-            margin : 170,
-            center : true,
-            smartSpeed :900,
-            nav:true,
-            navText: [
-                "<i class='fa carousel-left-arrow fa-chevron-left'></i>",
-                "<i class='fa carousel-right-arrow fa-chevron-right'></i>"
-            ],responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:true
-                },
-                600:{
-                    items:1,
-                    nav:true,
-                    margin : 120,
-                },
-                1000:{
-                    items:3,
-                    nav:true,
-                    loop:true,
-                    autoplay: true,
-                    autoplayTimeout: 1500,
-                    navText: [
-                        "<i class='fa carousel-left-arrow fa-chevron-left'></i>",
-                        "<i class='fa carousel-right-arrow fa-chevron-right'></i>"
-                    ],
-                }
-            }
-        });
+  $.fn.goTo = function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(this).offset().top + "px",
+      },
+      "slow"
+    );
+    return this; // for chaining...
+  };
 
-	};
-
-    $.fn.goTo = function() {
-        $('html, body').animate({
-            scrollTop: $(this).offset().top + 'px'
-        }, 'slow');
-        return this; // for chaining...
-    }
-
-	$(function(){
-		owlCarousel();
-	});
-
-
-}());
+  $(function () {
+    owlCarousel();
+  });
+})();
