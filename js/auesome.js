6174fabe82d9428a187b820014b5c4b6a5cb3e95
Anshul Gupta <hi.anshulg@gmail.com>
2020-07-31 11:09:43 -0700

    BS text

--- js/auesome.js
+++ js/auesome.js
@@ -13,3 +13,40 @@
     $("#sidebar").toggleClass("active");
   });
 })(jQuery);
+
+$(window).on("hashchange", function () {
+  hash = window.location.hash;
+  console.log(hash);
+  if ((hash = "#home")) {
+    $("p").last().html("hello");
+    console.log("hello");
+  }
+  if (hash == "#clothespin") {
+    $("#clothespin").addClass("your_class");
+    $("p").last().html("hi");
+  }
+  if (hash == "#spinner") {
+    $("p").last().html("hi");
+  }
+  if (hash == "#match") {
+    $("p").last().html("hi");
+  }
+  if (hash == "#flashcards") {
+    $("p").last().html("hi");
+  }
+  if (hash == "#texture") {
+    $("p").last().html("hi");
+  }
+  if (hash == "#threading") {
+    $("p").last().html("hi");
+  }
+  if (hash == "#lego") {
+    $("p").last().html("hi");
+  }
+  if (hash == "#zoob") {
+    $("p").last().html("zoooob");
+  }
+  if (hash == "#contact") {
+    $("p").last().html("hi");
+  }
+});
