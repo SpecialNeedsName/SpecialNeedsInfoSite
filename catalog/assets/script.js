d5a8819b277baadbc9a78b48233076eee9ad713e
Anshul Gupta <hi.anshulg@gmail.com>
2020-09-26 16:08:55 -0700

    catalog beta 1.

--- catalog/assets/script.js
+++ catalog/assets/script.js
@@ -16,6 +16,29 @@ $("#video").hide();
   });
 })(jQuery);
 
-$("button").click(function () {
-  $("p").hide();
+// $("button").click(function () {
+//   $("p").hide();
+// });
+
+$(window).on("hashchange", function () {
+  hash = window.location.hash;
+  console.log("changed");
+  $("#kit1").addClass("hideArea");
+  $("#kit2").addClass("hideArea");
+  $("#kit3").addClass("hideArea");
+  if (hash == "#kit1") {
+    console.log("on kit 1");
+    $("#kit1").removeClass("hideArea");
+  }
+  if (hash == "#kit2") {
+    console.log("on kit 2");
+    $("#kit2").removeClass("hideArea");
+  }
+
+  if (hash == "#kit3") {
+    console.log("on kit 3");
+    $("#kit3").removeClass("hideArea");
+
+    // $("#kit1").removeClass("hideArea");
+  }
 });
