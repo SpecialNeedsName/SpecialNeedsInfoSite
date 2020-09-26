c0aabc5e8a329a4c080d7ad5820d9ad82d0a320c
Anshul Gupta <hi.anshulg@gmail.com>
2020-09-25 20:42:13 -0700

    catalog + login

---
+++ catalog/assets/script.js
@@ -0,0 +1,21 @@
+$("#video").hide();
+
+(function ($) {
+  "use strict";
+
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
+
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+  });
+})(jQuery);
+
+$("button").click(function () {
+  $("p").hide();
+});
